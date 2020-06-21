92be8d9017c53c67c4b8b342d3844e38fd36ba3a
benjaminfriske <benjaminfriske@gmail.com>
2020-06-21 07:16:44 -0500

    Added waypoints prefab and EnemyPathing script.

---
+++ Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+    [SerializeField] List<Transform> waypoints;
+    private void Update()
+    {
+        
+    }
+
+}

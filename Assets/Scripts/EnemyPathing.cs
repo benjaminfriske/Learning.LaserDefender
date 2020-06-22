16df1a39adb63500466bfb9625b7e6c959180da4
benjaminfriske <benjaminfriske@gmail.com>
2020-06-22 06:53:10 -0500

    Added enemy path to move an object along a waypoint system.

--- Assets/Scripts/EnemyPathing.cs
+++ Assets/Scripts/EnemyPathing.cs
@@ -1,13 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class EnemyPathing : MonoBehaviour
 {
     [SerializeField] List<Transform> waypoints;
+    [SerializeField] float moveSpeed = 2f;
+
+    int waypointIndex = 0;
+
+
+    private void Start()
+    {
+        transform.position = waypoints.First().transform.position;
+    }
+
     private void Update()
     {
-        
+        Debug.Log(waypointIndex.ToString() + " : " + waypoints.Count.ToString());
+        if (waypointIndex <= waypoints.Count - 1)
+        {
+            Debug.Log("Here!");
+            var targetPosition = waypoints[waypointIndex].transform.position;
+            var moveThisFrame = moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, moveThisFrame);
+
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
 }
